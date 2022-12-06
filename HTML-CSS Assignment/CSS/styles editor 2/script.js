b190f41fd10c888e89f669e0452e11b25114a792
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-12-06 21:54:01 +0530

    Create script.js

---
+++ HTML-CSS Assignment/CSS/styles editor 2/script.js
@@ -0,0 +1,4 @@
+function apply(){
+    document.getElementById("main").style.left=`${document.getElementById("left").value}px`;
+    document.getElementById("main").style.top=`${document.getElementById("top").value}px`;
+}
